63796e29d4f88d870d9ddf96237f77b8aac8cfe4
MohamedSohil85 <mimo_mnb@hotmail.com>
2021-03-05 21:16:20 +0100

    push new codelines

---
+++ src/main/java/org/mohamed/dto/Project.java
@@ -0,0 +1,4 @@
+package org.mohamed.dto;
+
+public class Project {
+}
---
+++ src/main/java/org/mohamed/dto/Timeblock.java
@@ -0,0 +1,10 @@
+package org.mohamed.dto;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+import javax.persistence.Entity;
+
+@Entity
+
+public class Timeblock extends PanacheEntity {
+}
