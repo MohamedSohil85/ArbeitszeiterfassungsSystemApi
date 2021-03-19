fd8641aebbb8d7d34a540e44c96cc7fcb37f9c94
MohamedSohil85 <mimo_mnb@hotmail.com>
2021-03-19 02:56:12 +0100

    push new files

--- src/main/java/org/mohamed/dto/Vacation.java
+++ src/main/java/org/mohamed/dto/Vacation.java
@@ -2,8 +2,10 @@ package org.mohamed.dto;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
@@ -11,6 +13,7 @@ import javax.persistence.OneToOne;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class Vacation extends PanacheEntity {
 
     @OneToOne
